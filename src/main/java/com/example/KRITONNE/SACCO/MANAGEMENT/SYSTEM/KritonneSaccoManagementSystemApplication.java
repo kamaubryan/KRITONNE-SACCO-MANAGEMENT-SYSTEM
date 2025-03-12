96e63bbfabf0b05bd8b4f082d840c1dd4988130a
kamaubryan <briankamau344@gmail.com>
2025-03-12 12:10:07 +0300

    first commit

---
+++ src/main/java/com/example/KRITONNE/SACCO/MANAGEMENT/SYSTEM/KritonneSaccoManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.KRITONNE.SACCO.MANAGEMENT.SYSTEM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KritonneSaccoManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KritonneSaccoManagementSystemApplication.class, args);
+	}
+
+}
